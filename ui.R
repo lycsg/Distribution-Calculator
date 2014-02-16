95bfeec912f61f3a53ba64ed421b2f5b4ca52334
Robert Norberg <robertcnorberg@gmail.com>
2014-02-16 13:36:17 -0500

    Updated UI to include a selectInput to choose between binomial and Poisson Distribution. Added conditional panels to appear dcepending on distribution selected

--- ui.R
+++ ui.R
@@ -5,29 +5,45 @@ library(ggplot2)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Binomial Distribution"),
+  headerPanel("Distribution Calculator"),
   
   # Sidebar with a slider input for n and p
   sidebarPanel(
-    sliderInput("n", 
-                "Number of Trials:", 
-                min = 2,
-                max = 25, 
-                value = 10),
-    sliderInput("p", 
-                "Probability of Success:", 
-                min = 0,
-                max = 1,
-                step = .01,
-                value = .5, 
-                animate=animationOptions(interval=300, loop=T))
+    # Choose Distribution
+    selectInput('distribution', 
+                'Choose Distribution',
+                c('Binomial', 'Poisson')),
+    # Binomial Inputs
+    conditionalPanel(
+      condition="input.distribution == 'Binomial'",
+      # input binomial parameter "n"
+      sliderInput("n", 
+                  "Number of Trials:", 
+                  min = 2,
+                  max = 25, 
+                  value = 10),
+      # Input binomial parameter "p"
+      sliderInput("p", 
+                  "Probability of Success:", 
+                  min = 0,
+                  max = 1,
+                  step = .05,
+                  value = .5, 
+                  animate=animationOptions(interval=300, loop=T))
+    ),
+    
+    conditionalPanel(
+      condition="input.distribution == 'Poisson'",
+      sliderInput("lambda",
+                  "Lambda",
+                  min = 1,
+                  max = 15)
+    )
+    
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
-    tabsetPanel(
-      tabPanel('Binomial', plotOutput("myPlot")),
-      tabPanel('Poisson', plotOutput('myPlot2'))
-      )
+    plotOutput("myPlot")
   )
 ))
